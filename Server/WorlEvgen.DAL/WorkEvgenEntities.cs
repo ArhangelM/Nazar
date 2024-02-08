6390040aafd45df1258ed284738506a32cbbe8e0
ArhangelM <73641545+ArhangelM@users.noreply.github.com>
2024-02-08 13:38:58 +0200

    Init (#1)

---
+++ Server/WorlEvgen.DAL/WorkEvgenEntities.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Transactions;
+
+namespace WorlEvgen.DAL
+{
+    public class WorkEvgenEntities : DbContext, IWorkEvgenEntities
+    {
+        public WorkEvgenEntities(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            AddConfiguration(modelBuilder);
+
+            base.OnModelCreating(modelBuilder);
+
+            //Для теста
+            //GenerateUsers(modelBuilder);
+            //GenerateCardBalance(modelBuilder);
+            //GenerateTransactions(modelBuilder);
+        }
+
+        private void AddConfiguration(ModelBuilder modelBuilder)
+        {
+        }
+    
+    }
+}
